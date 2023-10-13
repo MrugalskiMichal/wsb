b70c21531efa7854d95b63c9a68231c253f27c0d
MrugalskiMichal <147168183+MrugalskiMichal@users.noreply.github.com>
2023-10-13 15:23:41 +0200

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Person
+    {
+        public string firstname;
+        public string lastname;
+        public float height;
+        public float weigth;
+
+        public string getdata()
+        {
+            return $"Imie i nazwisko; {firstname} {lastname}\nWzrost: {height} cm, waga {weigth} kg";
+        }
+    }
+}
---
+++ dane_osob.cs
@@ -0,0 +1,23 @@
+﻿using ConsoleApp2;
+namespace dane
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.firstname = "Janusz";
+            person.lastname = "Nowak";
+            person.weigth = 90.5F;
+            person.height = 192;
+            Console.WriteLine($"Dane użytkownika: \n{person.getdata()}");
+
+            Person kowal = new Person();
+            kowal.firstname = "Janusz";
+            kowal.lastname = "Nowak";
+            kowal.weigth = 90.5F;
+            kowal.height = 192;
+            Console.WriteLine($"Dane użytkownika: \n{kowal.getdata()}");
+        }
+    }
+}
